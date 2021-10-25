334315b141c2b9504288bc8be2cb18f762332ba0
jacksonPrimo <jackson.primo12@gmail.com>
2021-10-25 17:31:43 -0300

    added global style and change font family to roboto

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,12 +9,14 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel:"stylesheet", type:"text/css", href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;1,300&display=swap'}
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/styles/global.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
@@ -37,7 +39,13 @@ export default {
     icons: {
       solid: true,
       brands: true
-    }
+    },
+    imports: [
+      {
+        set: '@fortawesome/free-solid-svg-icons',
+        icons: ['fas']
+      },
+    ]
   },
 
   // Modules: https://go.nuxtjs.dev/config-modules

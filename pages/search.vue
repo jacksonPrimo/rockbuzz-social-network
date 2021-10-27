9baaa0f2d938a6220d151c4de823c0c01f2360e4
jacksonPrimo <jackson.primo12@gmail.com>
2021-10-27 19:27:00 -0300

    create search page

---
+++ pages/search.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{description}}
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  layout: 'default',
+  computed: {
+    description(){
+      return this.$route.query.description
+    }
+  }
+})
+</script>

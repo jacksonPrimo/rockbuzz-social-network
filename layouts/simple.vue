fb7ece96a32911160d10e8108354893e1c7e8d85
jacksonPrimo <jackson.primo12@gmail.com>
2021-10-27 19:26:01 -0300

    create layout simple

---
+++ layouts/simple.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full h-screen">
+    <Header/>
+    <div class="min-h-screen px-4 sm:px-6 lg:px-12 bg-gray-200">
+      <Nuxt/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Header from '@/components/Header.vue'
+export default Vue.extend({
+  components: { Header }
+})
+</script>

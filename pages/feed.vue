8199fc6bf4237d464d9f5f3dd1ffa61231dec7c4
jacksonPrimo <jackson.primo12@gmail.com>
2021-11-01 09:54:24 -0300

    adjusted tweeter component in feed page

--- pages/feed.vue
+++ pages/feed.vue
@@ -4,7 +4,7 @@
       <h1 class="text-3xl text-gray-500">Tweets</h1>
     </div>
     <div>
-      <Tweet v-for="(tweet, index) of tweets" :key="index" :tweet="tweet"/>
+      <Tweet v-for="(tweet, index) of tweets" :key="index" :tweet="tweet" :user-auth-id="userAuthId"/>
     </div>
   </div>
 </template>
@@ -14,11 +14,15 @@ import axios from '@/services/axios.service'
 export default Vue.extend({
   layout: 'default',
   data: ()=> ({
-    tweets: []
+    tweets: [],
+    userAuthId: ''
   }),
   mounted(){
     axios.get('/tweets').then(resp => {
       this.tweets = resp.data
+    })
+    axios.get('/auth/user-authenticated').then(resp => {
+      this.userAuthId = resp.data.user.id
     })
   },
 })

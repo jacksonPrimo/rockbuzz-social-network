7464f85b5071341215da31758049028c9b8dc0d1
jacksonPrimo <jackson.primo12@gmail.com>
2021-11-02 13:44:48 -0300

    created test to load routes

--- cypress/integration/initial.spec.js
+++ cypress/integration/initial.spec.js
@@ -1,8 +1,22 @@
 /// <reference types="cypress" />
 
-describe('Feed', ()=>{
-  it('search bar', ()=>{
+describe('Search', ()=>{
+  it('should redirect to feed', ()=>{
+    cy.visit('http://localhost:3000/')
+  })
+  it('should load feed', ()=>{
     cy.visit('http://localhost:3000/feed')
-    cy.get('[id=search]').type('publication name').type('{enter}')
+  })
+  it('should load feed', ()=>{
+    cy.visit('http://localhost:3000/feed')
+  })
+  it('should load search', ()=>{
+    cy.visit('http://localhost:3000/search')
+  })
+  it('should load profile', ()=>{
+    cy.visit('http://localhost:3000/profile/@jack_primo')
+  })
+  it('should redirect from profile to feed because nickName is empty', ()=>{
+    cy.visit('http://localhost:3000/profile/')
   })
 })
